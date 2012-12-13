38dbdbe428227aab7d847559397b3bfae4599513
Gal Steinitz <townsend-dev+pair+gal@googlegroups.com>
2012-12-13 11:59:52 -0800

    added decimal point to the calculator

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,16 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by developer on 12/13/12.
+//  Copyright (c) 2012 Gal Steinitz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+- (void)pushOperand:(double)operand;
+- (double)performOperation:(NSString *)operation;
+
+@end
--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -7,23 +7,58 @@
 //
 
 #import "CalculatorViewController.h"
+#import "CalculatorBrain.h"
 
 @interface CalculatorViewController ()
-
+@property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
+@property (nonatomic) BOOL userHasEnteredADecimalPoint;
+@property (nonatomic, strong) CalculatorBrain *brain;
 @end
 
 @implementation CalculatorViewController
 
-@synthesize display;
+@synthesize display = _display;
+@synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
+@synthesize userHasEnteredADecimalPoint = _userHasEnteredADecimalPoint;
+@synthesize brain = _brain;
 
-- (IBAction)sqrtPressed:(id)sender {
-    NSLog(@"sqrt was clicked, yo!");
+- (CalculatorBrain *)brain
+{
+    if (!_brain) _brain = [[CalculatorBrain alloc] init];
+    return _brain;
 }
- 
+
 - (IBAction)digitPressed:(UIButton *)sender {
     NSString *digit = [sender currentTitle];
-//    NSLog(@"user touched %@", digit);
-    self.display.text = [self.display.text stringByAppendingString:digit];
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        self.display.text = [self.display.text stringByAppendingString:digit];
+    } else {
+        self.display.text = digit;
+        self.userIsInTheMiddleOfEnteringANumber = YES;
+    }
+}
+
+- (IBAction)enterPressed {
+    [self.brain pushOperand:[self.display.text doubleValue]];
+    self.userIsInTheMiddleOfEnteringANumber = NO;
+    self.userHasEnteredADecimalPoint = NO;
+}
+
+- (IBAction)operationPressed:(UIButton *)sender {
+    
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        [self enterPressed];
+    }
+    NSString *operation = sender.currentTitle;
+    double result = [self.brain performOperation:operation];
+    self.display.text = [NSString stringWithFormat:@"%g", result];
+}
+
+- (IBAction)decimalPointPressed {
+    if (!self.userHasEnteredADecimalPoint) {
+        self.display.text = [self.display.text stringByAppendingString:@"."];
+        self.userHasEnteredADecimalPoint = YES;
+    }
 }
 
 @end
